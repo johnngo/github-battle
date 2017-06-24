dfb0b69e36b7f493a0504e85a015e179bbe1b3d4
John <johnngo@Johns-MBP.hitronhub.home>
2017-06-23 21:53:53 -0700

    react router

---
+++ app/components/battle.js
@@ -0,0 +1,46 @@
+var React = require('react');
+
+class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(id, username) {
+    this.setState(function() {
+      var newState = {};
+      newState[id +'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+      return newState
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div className ='row'>
+          {!playerOneName &&
+            <PlayerInput
+              id ='playerOne'
+              label = 'Player One'
+              onSubmit = {this.handleSubmit}
+              />}
+
+          {!playerTwoName &&
+            <PlayerInput
+            id ='playerOne'
+              label = 'Player One'
+              onSubmit = {this.handleSubmit}
+              />}
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = Battle;
